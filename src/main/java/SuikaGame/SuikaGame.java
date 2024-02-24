420721a9caec04354d202b3120ec3a62ebeb29f2
gustavogg23 <ggutierrez.7080@unimar.edu.ve>
2024-02-24 01:33:53 -0400

    h

---
+++ src/main/java/SuikaGame/SuikaGame.java
@@ -0,0 +1,11 @@
+package SuikaGame;
+
+public class SuikaGame {
+
+    public static void main(String[] args) {
+        MenuPrincipal menuPrincipal = new MenuPrincipal();
+        
+        menuPrincipal.setLocationRelativeTo(null);
+        menuPrincipal.setVisible(true);
+    }
+}
